d55ef6a02ab8ed1daf16f6f0efdc6b519db8e829
Digital-Jester <39645849+Digital-Jester@users.noreply.github.com>
2022-09-19 17:03:07 +1000

    Update stats.py
    New bit and update.

--- stats.py
+++ stats.py
@@ -44,10 +44,10 @@ SPI_DEVICE = 0
 # SPI_DEVICE = 0
 
 # 128x32 display with hardware I2C:
-disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
+# disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
 
 # 128x64 display with hardware I2C:
-# disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST)
+disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST)
 
 # Note you can change the I2C address by passing an i2c_address parameter like:
 # disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, i2c_address=0x3C)
@@ -118,17 +118,19 @@ while True:
     # Shell scripts for system monitoring from here : https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
     cmd = "hostname -I | cut -d\' \' -f1"
     IP = subprocess.check_output(cmd, shell = True )
-    cmd = "top -bn1 | grep load | awk '{printf \"CPU Load: %.2f\", $(NF-2)}'"
+    cmd = "top -bn1 | grep load | awk '{printf \"CPU: %.2f\", $(NF-2)}'"
     CPU = subprocess.check_output(cmd, shell = True )
     cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%sMB %.2f%%\", $3,$2,$3*100/$2 }'"
     MemUsage = subprocess.check_output(cmd, shell = True )
     cmd = "df -h | awk '$NF==\"/\"{printf \"Disk: %d/%dGB %s\", $3,$2,$5}'"
     Disk = subprocess.check_output(cmd, shell = True )
+    cmd = "vcgencmd measure_temp |cut -f 2 -d '='"
+    Temp = subprocess.check_output(cmd, shell = True )
 
     # Write two lines of text.
 
     draw.text((x, top),       "IP: " + str(IP,'utf-8'),  font=font, fill=255)
-    draw.text((x, top+8),     str(CPU,'utf-8'), font=font, fill=255)
+    draw.text((x, top+8),     str(CPU,'utf-8') + "  " + str(Temp,'utf-8'), font=font, fill=255)
     draw.text((x, top+16),    str(MemUsage,'utf-8'),  font=font, fill=255)
     draw.text((x, top+25),    str(Disk,'utf-8'),  font=font, fill=255)
 
