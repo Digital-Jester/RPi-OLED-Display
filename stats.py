f8bedd3adc6668dcb787d1993f71956d5f1306a3
Digital-Jester <39645849+Digital-Jester@users.noreply.github.com>
2022-09-22 12:30:18 +1000

    bug fix

--- stats.py
+++ stats.py
@@ -32,6 +32,7 @@ import subprocess
 import RPi.GPIO as GPIO
 
 BUTTON_GPIO = 4
+BUTTON_PRESSED = False
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(BUTTON_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -109,11 +110,14 @@ font = ImageFont.truetype('PixelOperator.ttf', 16)
 
 while True:
     # button is pressed when pin is LOW
-    if not GPIO.input(BUTTON_GPIO):
+    if not GPIO.input(BUTTON_GPIO) and not BUTTON_PRESSED:
+        BUTTON_PRESSED = True
         showpage += 1
-        print("Page Inc")
+        #print("Page Inc")
         if showpage > showpagemax:
             showpage = 1
+    else:
+            BUTTON_PRESSED = False
 
     # Draw a black filled box to clear the image.
     draw.rectangle((0,0,width,height), outline=0, fill=0)
